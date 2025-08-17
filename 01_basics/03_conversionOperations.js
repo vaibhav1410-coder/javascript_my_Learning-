136f39a4fa183634f616d735a3fcc1bf5af48fda
Vaibhav <85795342+vaibhav1410-coder@users.noreply.github.com>
2025-08-17 16:29:09 +0530

    Update 03_conversionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -9,7 +9,7 @@ let valueInNumber = Number(score)
 
 
 // "33" => 33
-// "33abc" => NaN
+// "33abc" => NaN   undefined => NaN null=> NaN    // type is always Number after conversion   
 //  true => 1; false => 0
 
 let isLoggedIn = "vaibhav"
@@ -18,14 +18,18 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
-// "" => false
+// "" => false     
 // "hitesh" => true
+//undefined => false    null=> false  
+//123 => true
 
 let someNumber = 33
 
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// 33 => 33 and type is String
 
 // *********************** Operations ***********************
 
